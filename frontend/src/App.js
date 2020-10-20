86a73d291d612701a1f941303e04cb64913b1226
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-20 15:45:37 +0700

    Empty React app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Добро пожаловать в магазин IGadgetShop</h1>
+    </>
+  );
+}
+
+export default App;
