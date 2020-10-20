57184050d22ea0ca8f4a751ffec2df76645083b4
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-20 16:26:26 +0700

    Frontend: main page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <h1>Добро пожаловать в магазин IGadgetShop</h1>
+      <div className="grid-container">
+        <Header />
+        <Sidebar />
+        <main>
+          <h1>Добро пожаловать в магазин IGadgetShop</h1>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+      };
+
+    return (
+        <header className="header">
+            <div className="header__brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">IGadgetShop</a>
+          </div>
+          <div className="header__links">
+            <a href="cart.html">Корзина</a>
+            <a href="login.html">Регистрация</a>
+          </div>
+        </header>
+    )
+};
+
+export default Header;
