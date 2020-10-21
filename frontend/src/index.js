84b2eb0c3c45a8ac0fd77195edd13d6f1db7643a
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-21 16:55:45 +0700

    lib bootstrap scss => css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import './styles/common/bootstrap.min.scss';
+import './styles/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
