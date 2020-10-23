19f51f15349bb018f5579f7522a69d064bdbf5ba
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-24 02:58:13 +0700

    Get products and productsById data from MongoDB (product.routes)

---
+++ backend/routes/product.routes.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel'
+
+const router = express.Router()
+
+// @desc     Get all products
+// @route    GET /api/products
+// @access   Public
+router.get("/", asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+// @desc     Get product by Id
+// @route    GET /api/products/:id
+// @access   Public
+router.get("/:id", asyncHandler(async (req, res) => {
+    const productId = req.params.id
+    const product = await Product.findById(productId)
+    if (product)
+      res.json(product)
+    else
+      res.status(404).json({msg: "Товар не найден"})
+}))
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db"
-import products from './data/products'
+
+import productRoutes from './routes/product.routes'
 
 dotenv.config()
 
@@ -16,18 +17,7 @@ app.get("/", (req, res) => {
     res.send("API запущен...")
 })
 
-app.get("/api/products", (req, res) => {
-    res.json(products)
-})
-
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id
-    const product = products.find(p => p._id === productId)
-    if (product)
-      res.json(product)
-    else
-      res.status(404).json({msg: "Товар не найден"})
-})
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, 
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold) 
