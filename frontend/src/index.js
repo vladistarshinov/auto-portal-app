6daa73ce1ef5be3070f9906f10b062e2c9fa150a
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-24 18:07:56 +0700

    Frontend: Redux (store)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './styles/index.scss';
 import './styles/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
