c7a80f2af213c72b6b12fa8093aff720af504620
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-10-26 20:30:58 +0700

    Backend: jwt util, user.controller(token)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/user.model';
+import generateToken from '../utils/jwt-gen';
 
 const userController = {};
 
@@ -17,7 +18,7 @@ userController.authUser = asyncHandler(async (req, res) => {
                 name: user.name,
                 email: user.name,
                 isAdmin: user.isAdmin,
-                token: null
+                token: generateToken(user._id)
             });
         } else {
             res.status(401);
