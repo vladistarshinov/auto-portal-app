a2afa3c76f10a9a32c38e6e5476fe0fc2bc5c056
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2022-01-05 22:59:53 +0300

    FE: mui: Home

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,73 +1,104 @@
 import React, { useEffect } from "react";
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import Link from "@mui/material/Link";
+import { useDispatch, useSelector } from "react-redux";
 import ProductList from "../components/ProductList";
-import { listOfProduct } from '../redux/actions/product.actions';
-import Loader from '../ui/components/Loader';
-import Message from '../components/Message';
-import Pagination from '../components/Pagination';
-import TopProductsCarousel from '../components/TopProductsCarousel';
-import MetaHeader from '../components/MetaHeader';
-import { Row, Col } from 'bootstrap-4-react';
-import styled from 'styled-components';
+import { listOfProduct } from "../redux/actions/product.actions";
+import Loader from "../ui/components/Loader";
+import Message from "../components/Message";
+import Pagination from "../components/Pagination";
+import TopProductsCarousel from "../components/TopProductsCarousel";
+import MetaHeader from "../components/MetaHeader";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import UndoIcon from "@mui/icons-material/Undo";
 
 const Home = ({ match }) => {
-    const keyword = match.params.keyword;
-    const pageNumber = match.params.pageNumber || 1;
+  const keyword = match.params.keyword;
+  const pageNumber = match.params.pageNumber || 1;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const productList = useSelector(state => state.productList);
-    const { loading, products, page, pages, error } = productList;
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, page, pages, error } = productList;
 
-    useEffect(() => {
-        dispatch(listOfProduct(keyword, pageNumber));
-    }, [dispatch, keyword, pageNumber]);
+  useEffect(() => {
+    dispatch(listOfProduct(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
-    const Heading = styled.h3`
-        color: #070049;
-        text-align: center;
-        margin-top: 2rem;
-        margin-bottom: 1rem;
-    `;
+  const Heading = styled(Typography)`
+    color: #070049;
+    text-align: center;
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+    font-size: 2rem;
+  `;
 
-    return (
-      <>
-        <MetaHeader />
-        {!keyword ? (
-          <>
-            <Heading>Лучшие товары</Heading>
-            <TopProductsCarousel />
-          </>
-        ) : (
-          <Link to="/" className="btn btn-light">
+  return (
+    <>
+      <MetaHeader />
+      {!keyword ? (
+        <>
+          <Heading>Лучшие товары</Heading>
+          <TopProductsCarousel />
+        </>
+      ) : (
+        <Link
+          href="/"
+          sx={{
+            textDecoration: "none",
+          }}
+        >
+          <Button
+            color="inherit"
+            sx={{
+              mt: 2,
+              color: "text.muted",
+              "&:focus": {
+                outline: "none",
+              },
+            }}
+          >
+            <UndoIcon sx={{ mr: 1 }} />
             Назад
-          </Link>
-        )}
-        <Heading>Ассортимент товаров</Heading>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          // alert(`${error}`)
-          <Message variant="error">{error}</Message>
-        ) : (
-          <>
-            <Row>
-              {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <ProductList product={product} />
-                </Col>
-              ))}
-            </Row>
-            <Pagination
-              pages={pages}
-              page={page}
-              keyword={keyword ? keyword : ""}
-            />
-          </>
-        )}
-      </>
-    );
-}
+          </Button>
+        </Link>
+      )}
+      <Heading>Ассортимент товаров</Heading>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        // alert(`${error}`)
+        <Message variant="error">{error}</Message>
+      ) : (
+        <>
+          <Box>
+            {products.map((product) => (
+              <Grid
+                display="inline-grid"
+                container
+                direction="row"
+                key={product._id}
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+              >
+                <ProductList product={product} />
+              </Grid>
+            ))}
+          </Box>
+          <Pagination
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 export default Home;
