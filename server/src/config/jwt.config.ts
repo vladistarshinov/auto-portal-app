eb51aacf6b79066bd8cd6733026d81aa573fb8d7
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2022-05-10 08:06:59 +0300

    Merge

--- server/src/config/jwt.config.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtModuleOptions } from '@nestjs/jwt';
-
-export const getJwtConfig = async (
-  configService: ConfigService
-): Promise<JwtModuleOptions> => ({
-  secret: configService.get('JWT_SECRET'),
-});
--- server/src/config/mongo.config.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { TypegooseModuleOptions } from 'nestjs-typegoose';
-
-export const getMongoDbConfig = async (
-  configService: ConfigService
-): Promise<TypegooseModuleOptions> => ({
-  uri: configService.get('MONGO_URI'),
-});
